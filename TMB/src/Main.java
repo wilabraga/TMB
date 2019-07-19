9e746195176b9404697bc1e1ce992e1dcb727558
bragalotwill <wmbraga7@gmail.com>
2019-07-19 10:16:46 +0200

    Added Backend Query Functions

--- TMB/src/Main.java
+++ TMB/src/Main.java
@@ -1,9 +1,11 @@
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		TMB tmb = new TMB();
-		tmb.accessTMB();
+		TMB.accessTMB();
+		ArrayList<Object[]> result = TMB.executeQuery("SELECT * FROM USER", "ID", "first_name", "minit", "last_name", "password", "passenger_email");
+		TMB.printResult(result);
 	}
 }
--- TMB/src/TMB.java
+++ TMB/src/TMB.java
@@ -3,18 +3,43 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class TMB {
+	
+	private static Connection conn;
+	private static Statement statement;
 
-	public void accessTMB() throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/tmb", "root", "root");
-		Statement statement = conn.createStatement();
-		ResultSet resultset = statement.executeQuery("select * from user");
-		while(resultset.next()) {
-			System.out.println(resultset.getString(2));
+	public static void accessTMB() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/tmb", "root", "root");
+		statement = conn.createStatement();
+	}
+	
+	public static ArrayList<Object[]> executeQuery(String query, String... attributes) throws SQLException {
+		ResultSet resultSet = statement.executeQuery(query);
+		ArrayList<Object[]> result = new ArrayList<>();
+		while (resultSet.next()) {
+			Object[] tuple = new Object[attributes.length];
+			for(int i = 0; i < tuple.length; i++) {
+				tuple[i] = resultSet.getObject(attributes[i]);
+			}
+			result.add(tuple);
 		}
-		resultset.close();
-		System.out.println("yeet");
+		resultSet.close();
+		return result;
+	}
+	
+	public static void executeModification(String modify) throws SQLException {
+		statement.execute(modify);
+	}
+	
+	public static void printResult(ArrayList<Object[]> result) {
+		for (Object[] arr: result) {
+			for (Object o: arr) {
+				System.out.print(o + "\t");
+			}
+			System.out.println();
+		}
 	}
 }
