1330577d5ae879d7932b1f6abe90d1202f6bd22d
bragalotwill <wmbraga7@gmail.com>
2019-07-19 21:51:28 +0200

    morrrreeee

--- TMB/src/Queries.java
+++ TMB/src/Queries.java
@@ -1,6 +1,8 @@
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
 public class Queries {
@@ -210,7 +212,7 @@ public class Queries {
 	
 	public static int[] getAvgRatings(String stationName) {
 		String query = ""
-				+ "SELECT AVG(shopping), AVG(connection_speed) "
+				+ "SELECT AVG(shopping) AS avg_shopping, AVG(connection_speed) AS avg_speed "
 				+ "FROM review "
 				+ "WHERE station_name = (?) AND approval_status = 'Approved';";
 		ArrayList<int[]> ratings = new ArrayList<>();
@@ -220,7 +222,198 @@ public class Queries {
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		Object[] result = TMB.executePreparedQuery("line_name").get(0);
+		Object[] result = TMB.executePreparedQuery("avg_shopping", "avg_speed").get(0);
 		return new int[] {(int) result[0], (int) result[1]};
+	}
+	
+	public static int getNumStops(String lineName) {
+		String query = ""
+				+ "SELECT COUNT(station_name) AS num_stations"
+				+ "FROM station_on_line "
+				+ "WHERE line_name = (?);";
+		PreparedStatement psmt = TMB.makePreparedStatement(query);
+		try {
+			psmt.setString(1, lineName);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		Object[] result = TMB.executePreparedQuery("num_stations").get(0);
+		return (int) result[0];
+	}
+	
+	public static ArrayList<Object[]> getStationsFromLine(String lineName, String order) {
+		String query = ""
+				+ "SELECT station_name, order_number "
+				+ "FROM station_on_line "
+				+ "WHERE line_name = (?) "
+				+ "ORDER BY (?) ASC;";
+		ArrayList<Object[]> stations = new ArrayList<>();
+		PreparedStatement psmt = TMB.makePreparedStatement(query);
+		try {
+			psmt.setString(1, lineName);
+			psmt.setString(2, order);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return TMB.executePreparedQuery("station_name", "order_number");
+	}
+	
+	public static Object[] getUserInfo(String ID, String... attributes) {
+		String query = ""
+				+ "SELECT *"
+				+ "FROM user "
+				+ "WHERE ID = (?);";
+		PreparedStatement psmt = TMB.makePreparedStatement(query);
+		try {
+			psmt.setString(1, ID);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return TMB.executePreparedQuery(attributes).get(0);
+	}
+	
+	public static void updateUser(String originalID, String newID, String fName, String mInit, String lName, String password, String email) {
+		String query = ""
+				+ "UPDATE user "
+				+ "SET ID = (?), first_name = (?), last_name = (?), password = (?), passenger_email = (?) "
+				+ "WHERE ID = (?);";
+		PreparedStatement psmt = TMB.makePreparedStatement(query);
+		try {
+			psmt.setString(1, newID);
+			psmt.setString(2, fName);
+			psmt.setString(3, mInit);
+			psmt.setString(4, lName);
+			psmt.setString(5, password);
+			psmt.setString(6, email);
+			psmt.setString(7, originalID);
+			
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		TMB.executePreparedModification();
+	}
+	
+	public static void deleteUserAccount(String ID) {
+		String query = ""
+				+ "DELETE FROM user "
+				+ "WHERE ID = (?);";
+		PreparedStatement psmt = TMB.makePreparedStatement(query);
+		try {
+			psmt.setString(1, ID);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		TMB.executePreparedModification();
+		query = ""
+				+ "DELETE FROM review "
+				+ "WHERE passenger_ID = (?);";
+		psmt = TMB.makePreparedStatement(query);
+		try {
+			psmt.setString(1, ID);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		TMB.executePreparedModification();
+		query = ""
+				+ "DELETE FROM card "
+				+ "WHERE user_ID = (?);";
+		psmt = TMB.makePreparedStatement(query);
+		try {
+			psmt.setString(1, ID);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		TMB.executePreparedModification();
+		query = ""
+				+ "DELETE FROM trip "
+				+ "WHERE user_ID = (?);";
+		psmt = TMB.makePreparedStatement(query);
+		try {
+			psmt.setString(1, ID);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		TMB.executePreparedModification();
+	}
+	
+	public static void buyCard(String ID, String type, Timestamp purchaseDate, int usesLeft, Date expirationDate) {
+		String query = ""
+				+ "INSERT INTO card "
+				+ "VALUES ((?), (?), (?), (?), (?));";
+		PreparedStatement psmt = TMB.makePreparedStatement(query);
+		try {
+			psmt.setString(1, ID);
+			psmt.setString(2, type);
+			psmt.setTimestamp(3, purchaseDate);
+			psmt.setInt(4, usesLeft);
+			psmt.setDate(5, expirationDate);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		TMB.executePreparedModification();
+	}
+	
+	public static ArrayList<Object[]> getUserValidCards(String ID, String... attributes) {
+		String query = ""
+				+ "SELECT * "
+				+ "FROM card "
+				+ "WHERE user_ID = (?) AND uses_left > 0 AND expiration_date >= CURDATE();";
+		PreparedStatement psmt = TMB.makePreparedStatement(query);
+		try {
+			psmt.setString(1, ID);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return TMB.executePreparedQuery(attributes);
+	}
+	
+	public static void embark(String ID, String type, Timestamp purchaseDate, Timestamp startTime, Timestamp endTime, String startStation, String endStation) {
+		String query = ""
+				+ "UPDATE card "
+				+ "SET uses_left = uses_left - 1 "
+				+ "WHERE user_ID = (?) AND type = (?) AND purchase_date_time = (?));";
+		PreparedStatement psmt = TMB.makePreparedStatement(query);
+		try {
+			psmt.setString(1, ID);
+			psmt.setString(2, type);
+			psmt.setTimestamp(3, purchaseDate);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		TMB.executePreparedModification();
+		query = ""
+				+ "INSERT INTO trip "
+				+ "VALUES ((?), (?), (?), (?), (?), (?), (?));";
+		psmt = TMB.makePreparedStatement(query);
+		try {
+			psmt.setString(1, ID);
+			psmt.setString(2, type);
+			psmt.setTimestamp(3, purchaseDate);
+			psmt.setTimestamp(4, startTime);
+			psmt.setTimestamp(5, endTime);
+			psmt.setString(6, startStation);
+			psmt.setString(7, endStation);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		TMB.executePreparedModification();
+	}
+	
+	public static ArrayList<Object[]> getUserValidCards(String ID, String type, Timestamp purchaseDate, String order, String... attributes) {
+		String query = ""
+				+ "SELECT * "
+				+ "FROM trip "
+				+ "WHERE user_ID = (?) AND type = (?) AND purchase_date_time = (?)) "
+				+ "ORDER BY (?) ASC;";
+		PreparedStatement psmt = TMB.makePreparedStatement(query);
+		try {
+			psmt.setString(1, ID);
+			psmt.setString(2, type);
+			psmt.setTimestamp(3, purchaseDate);
+			psmt.setString(4, type);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return TMB.executePreparedQuery(attributes);
 	}
 }
